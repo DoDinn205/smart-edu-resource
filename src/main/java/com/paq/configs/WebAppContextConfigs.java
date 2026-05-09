3528850c5dcc7de841f524061b82ffb0a2d73012
Pham Quyen <paqvippro@gmail.com>
2026-05-09 16:36:17 +0700

    Initial Project

---
+++ src/main/java/com/paq/configs/WebAppContextConfigs.java
@@ -0,0 +1,32 @@
+package com.paq.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author paqvi
+ */
+@Configuration
+@ComponentScan(
+        basePackages = {
+            "com.paq.controllers"
+        }
+)
+@EnableWebMvc
+public class WebAppContextConfigs implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+    
+}
---
+++ src/main/java/com/paq/controllers/HomeController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.paq.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author paqvi
+ */
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String inde() {
+        return "index";
+    }
+}
