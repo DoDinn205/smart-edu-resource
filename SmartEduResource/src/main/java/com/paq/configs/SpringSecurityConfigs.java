6c2ecdc4c9b6660b1b4d1b25ee8fc30211095b06
Pham Quyen <paqvippro@gmail.com>
2026-06-01 09:37:07 +0700

    refactor: remove unused JWT filter and related security configurations

--- SmartEduResource/src/main/java/com/paq/configs/SpringSecurityConfigs.java
+++ SmartEduResource/src/main/java/com/paq/configs/SpringSecurityConfigs.java
@@ -16,10 +16,6 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import com.paq.filters.JwtFilter;
-import com.paq.repository.UserRepository;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -39,9 +35,6 @@ public class SpringSecurityConfigs {
     @Autowired
     private Environment env;
 
-    @Autowired
-    private UserRepository userRepository;
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -53,30 +46,6 @@ public class SpringSecurityConfigs {
     }
 
     @Bean
-    public JwtFilter jwtFilter() {
-        return new JwtFilter(userRepository);
-    }
-
-    @Bean
-    @Order(1)
-    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .securityMatcher("/api/secure/**")
-                .csrf(c -> c.disable())
-                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                .anyRequest().authenticated()
-                )
-                .addFilterBefore(
-                        jwtFilter(),
-                        UsernamePasswordAuthenticationFilter.class
-                );
-
-        return http.build();
-    }
-
-    @Bean
-    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.securityMatcher("/admin/**", "/", "/login").csrf(c -> c.disable()).authorizeHttpRequests((requests) -> requests
                 .requestMatchers(
